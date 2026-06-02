8a6232fa2bfd70f447c34c1bc413e5e489b84f87
srinivash <unitofsrinivash@gmail.com>
2026-06-02 23:16:53 +0530

    Add file upload middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Upload an image
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: auto,
+    });
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temp file as op got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
