bcee7a7baa1642d86785bfd22a026b1c1ca0cb4c
srinivash <unitofsrinivash@gmail.com>
2026-06-03 09:32:37 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,8 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler()).catch((error) => next(error));
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
   };
 };
 
