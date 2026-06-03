9248c77016d49ee5c77125b8a117432abf4e29fc
srinivash <unitofsrinivash@gmail.com>
2026-06-04 00:53:06 +0530

    Add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -87,4 +87,4 @@ userSchema.methods.generateRefreshToken = function () {
   );
 };
 
-export const Users = mongoose.model("Users", userSchema);
+export const User = mongoose.model("User", userSchema);
